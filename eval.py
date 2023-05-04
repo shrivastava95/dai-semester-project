ad7011c8366df3d54d18d9ad9238a19b69c135a2
Ishaan Shrivastava <shrivastava.9@iitj.ac.in>
2023-05-04 06:21:19 +0530

    add osmething

--- eval.py
+++ eval.py
@@ -4,7 +4,7 @@ import os
 import sys
 import argparse
 from importlib import import_module
-from train_eval import train, val,  test, Logger #!
+from train_eval import test, Logger #!
 import shutil
 import time
 
