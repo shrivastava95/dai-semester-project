57e3b99c921c74bdc4b7a307c8572dfb84118cc9
Ishaan Shrivastava <shrivastava.9@iitj.ac.in>
2023-05-08 18:19:24 +0530

    start the run with CE_L3

--- train_cel.py
+++ train_cel.py
@@ -7,9 +7,9 @@ from torch import optim
 from torch.nn import DataParallel
 from tqdm import tqdm
 
-name_text = 'CE_L2'
+name_text = 'CE_L3'
 enable_ln_loss = True
-N = 2
+N = 3
 
 def train(epoch, net, loss_fn, data_loader, optimizer, get_lr, requires_control = True):
     ## log outputs to noise folder. store the latest epoch only
